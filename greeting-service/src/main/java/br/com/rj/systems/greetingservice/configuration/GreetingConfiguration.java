fd91edda6f34257bff4e94087ca5688b46b58a67
RichardJd <jdr.richard@gmail.com>
2022-03-01 17:59:22 -0300

    Add configuration files

--- greeting-service/src/main/java/br/com/rj/systems/greetingservice/configuration/GreetingConfiguration.java
+++ greeting-service/src/main/java/br/com/rj/systems/greetingservice/configuration/GreetingConfiguration.java
@@ -2,7 +2,9 @@ package br.com.rj.systems.greetingservice.configuration;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.ConstructorBinding;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 
+@RefreshScope
 @ConstructorBinding
 @ConfigurationProperties("greeting-service")
 public record GreetingConfiguration(
