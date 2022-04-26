fb51e1b54ba79c455f9cf99aa6e8c52ddbae6154
RichardJd <jdr.richard@gmail.com>
2022-04-26 18:43:12 -0300

    Distributed tracing com docker, zipkin, eureka e sleuth

--- cambio-service/src/main/java/br/com/rj/systems/cambioservice/controller/CambioController.java
+++ cambio-service/src/main/java/br/com/rj/systems/cambioservice/controller/CambioController.java
@@ -5,6 +5,7 @@ import br.com.rj.systems.cambioservice.repository.CambioRepository;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 
+@Slf4j
 @Tag(name = "Cambio endpoint")
 @RequiredArgsConstructor
 @RestController
@@ -31,6 +33,7 @@ public class CambioController {
             @PathVariable String from,
             @PathVariable String to
     ) {
+        log.info("getCambio is called with: {}, {} and {}", amount, from, to);
         var cambio = cambioRepository.findByFromAndTo(from, to)
                 .orElseThrow(() -> new RuntimeException("CurrencyUnsupported"));
 
